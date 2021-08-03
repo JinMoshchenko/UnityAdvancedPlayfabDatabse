4e67234268e10c58c2aba2769d5284dcedbd551f
JinMoshchenko <jinmoshchenko@gmail.com>
2021-08-03 21:16:36 +0300

    wwww

--- UnityAdvancedPlayfab/Assets/Scripts/Game.cs
+++ UnityAdvancedPlayfab/Assets/Scripts/Game.cs
@@ -2,24 +2,43 @@
 using UnityEngine.SceneManagement;
 using TMPro;
 
+public class Character
+{
+    public int _STRENGTH, _AGILITY, _STAMINA, _KNOWLEDGE, _ABILITIES;
+
+    public Character(int _STRENGTH, int _AGILITY, int _STAMINA, int _KNOWLEDGE, int _ABILITIES)
+    {
+        this._STRENGTH = _STRENGTH;
+        this._AGILITY = _AGILITY;
+        this._STAMINA = _STAMINA;
+        this._KNOWLEDGE = _KNOWLEDGE;
+        this._ABILITIES = _ABILITIES;
+    }
+}
+
 public class Game : MonoBehaviour
 {
     [SerializeField] private Transform player;
     [SerializeField] private GameObject logo;
     [SerializeField] private GameObject winLogo;
 
+    #region Public fields
     public TextMeshProUGUI STRENGTH_text;
     public TextMeshProUGUI AGILITY_text;
     public TextMeshProUGUI STAMINA_text;
     public TextMeshProUGUI KNOWLEDGE_text;
     public TextMeshProUGUI ABILITIES_text;
+    public PlayfabManager playfabManager;
+    [HideInInspector]public int STRENGTH, AGILITY, STAMINA, KNOWLEDGE, ABILITIES;
+    #endregion
 
-    int STRENGTH=0, AGILITY=0, STAMINA=0, KNOWLEDGE=0, ABILITIES=0;
+    #region Private fields
     const int goalScore = 100;
     float goalScale = 0.7318307f;
     float goalYposition = -0.88f;
     float scaleMultiplier;
     float yPosMultiplier;
+    #endregion
 
 
     private void Start()
@@ -27,12 +46,33 @@ public class Game : MonoBehaviour
         CalculateMultipliers();
     }
 
+    public Character ReturnClass()
+    {
+        return new Character(STRENGTH, AGILITY, STAMINA, KNOWLEDGE, ABILITIES);
+    }
     public void CalculateMultipliers()
     {
         scaleMultiplier = (goalScale - player.localScale.x) / (goalScore * 5);
         yPosMultiplier = (player.position.y - goalYposition) / (goalScore * 5);
     }
+    public void GameCompleted()
+    {
+        if (STRENGTH == goalScore && AGILITY == goalScore && STAMINA == goalScore && KNOWLEDGE == goalScore && ABILITIES == goalScore)
+        {
+            logo.SetActive(false);
+            winLogo.SetActive(true);
+        }
+    }
+    public void SetUIValues(Character character)
+    {
+        STRENGTH_text.SetText("{0}/{1}", character._STRENGTH, goalScore);
+        AGILITY_text.SetText("{0}/{1}", character._AGILITY, goalScore);
+        STAMINA_text.SetText("{0}/{1}", character._STAMINA, goalScore);
+        KNOWLEDGE_text.SetText("{0}/{1}", character._KNOWLEDGE, goalScore);
+        ABILITIES_text.SetText("{0}/{1}", character._ABILITIES, goalScore);
+    }
 
+    #region Buttons
     public void STRENGTH_B()
     {
         if(STRENGTH < goalScore)
@@ -42,11 +82,7 @@ public class Game : MonoBehaviour
             player.localScale = new Vector3(player.localScale.x + scaleMultiplier, player.localScale.y + scaleMultiplier, player.localScale.z + scaleMultiplier);
             player.position = new Vector3(player.position.x, player.position.y - yPosMultiplier, player.position.y);
         }
-        if(STRENGTH == goalScore && AGILITY == goalScore && STAMINA == goalScore && KNOWLEDGE == goalScore && ABILITIES == goalScore)
-        {
-            logo.SetActive(false);
-            winLogo.SetActive(true);
-        }
+        GameCompleted();
     }
     public void AGILITY_B()
     {
@@ -57,11 +93,7 @@ public class Game : MonoBehaviour
             player.localScale = new Vector3(player.localScale.x + scaleMultiplier, player.localScale.y + scaleMultiplier, player.localScale.z + scaleMultiplier);
             player.position = new Vector3(player.position.x, player.position.y - yPosMultiplier, player.position.y);
         }
-        if (STRENGTH == goalScore && AGILITY == goalScore && STAMINA == goalScore && KNOWLEDGE == goalScore && ABILITIES == goalScore)
-        {
-            logo.SetActive(false);
-            winLogo.SetActive(true);
-        }
+        GameCompleted();
     }
     public void STAMINA_B()
     {
@@ -72,11 +104,7 @@ public class Game : MonoBehaviour
             player.localScale = new Vector3(player.localScale.x + scaleMultiplier, player.localScale.y + scaleMultiplier, player.localScale.z + scaleMultiplier);
             player.position = new Vector3(player.position.x, player.position.y - yPosMultiplier, player.position.y);
         }
-        if (STRENGTH == goalScore && AGILITY == goalScore && STAMINA == goalScore && KNOWLEDGE == goalScore && ABILITIES == goalScore)
-        {
-            logo.SetActive(false);
-            winLogo.SetActive(true);
-        }
+        GameCompleted();
     }
     public void KNOWLEDGE_B()
     {
@@ -87,11 +115,7 @@ public class Game : MonoBehaviour
             player.localScale = new Vector3(player.localScale.x + scaleMultiplier, player.localScale.y + scaleMultiplier, player.localScale.z + scaleMultiplier);
             player.position = new Vector3(player.position.x, player.position.y - yPosMultiplier, player.position.y);
         }
-        if (STRENGTH == goalScore && AGILITY == goalScore && STAMINA == goalScore && KNOWLEDGE == goalScore && ABILITIES == goalScore)
-        {
-            logo.SetActive(false);
-            winLogo.SetActive(true);
-        }
+        GameCompleted();
     }
     public void ABILITIES_B()
     {
@@ -102,14 +126,15 @@ public class Game : MonoBehaviour
             player.localScale = new Vector3(player.localScale.x + scaleMultiplier, player.localScale.y + scaleMultiplier, player.localScale.z + scaleMultiplier);
             player.position = new Vector3(player.position.x, player.position.y - yPosMultiplier, player.position.y);
         }
-        if (STRENGTH == goalScore && AGILITY == goalScore && STAMINA == goalScore && KNOWLEDGE == goalScore && ABILITIES == goalScore)
-        {
-            logo.SetActive(false);
-            winLogo.SetActive(true);
-        }
+        GameCompleted();
     }
     public void BACK_B()
     {
         SceneManager.LoadScene(0);
     }
+    public void SAVE_B()
+    {
+        playfabManager.SaveCharacterStats();
+    }
+    #endregion
 }
--- UnityAdvancedPlayfab/Assets/Scripts/PlayfabManager.cs
+++ UnityAdvancedPlayfab/Assets/Scripts/PlayfabManager.cs
@@ -1,14 +1,18 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using PlayFab;
 using PlayFab.ClientModels;
+using Newtonsoft.Json;
 
 public class PlayfabManager : MonoBehaviour
 {
     public InputField emailInput;
     public InputField passwordInput;
     public Text message;
+    private Character character;
+    private Game game;
 
 
     void Start()
@@ -28,12 +32,11 @@ public class PlayfabManager : MonoBehaviour
     void OnSuccess(LoginResult result)
     {
         message.text = "Welcome!";
-        Debug.Log("Successful logic/account create!");
+        Debug.Log("Successful login with unique device!");
     }
     void OnError(PlayFabError error)
     {
         message.text = error.ErrorMessage;
-        Debug.Log("Error while logging in/creating account!");
         Debug.Log(error.GenerateErrorReport());
     }
 
@@ -55,7 +58,6 @@ public class PlayfabManager : MonoBehaviour
     }
     void OnRegisterSuccess(RegisterPlayFabUserResult result)
     {
-        message.text = "Registered and logged in!";
         Debug.Log("Registered and logged in!");
         SceneManager.LoadScene(1);
     }
@@ -73,8 +75,8 @@ public class PlayfabManager : MonoBehaviour
     }
     void OnLoginSuccess(LoginResult result)
     {
-        message.text = "Logged in!";
         Debug.Log("Successful login!");
+        GetCharacterStats();
         SceneManager.LoadScene(1);
     }
     #endregion
@@ -92,6 +94,43 @@ public class PlayfabManager : MonoBehaviour
     void OnPasswordReset(SendAccountRecoveryEmailResult result)
     {
         message.text = "Password reset mail sent!";
+    }
+    #endregion
+
+    #region GameData
+    public void SaveCharacterStats()
+    {
+        var request = new UpdateUserDataRequest
+        {
+            Data = new Dictionary<string, string>
+            {
+                {"Stats", JsonConvert.SerializeObject(GameObject.Find("GameContainer").GetComponent<Game>().ReturnClass())}
+            }
+        };
+        PlayFabClientAPI.UpdateUserData(request, OnCharacterDataSend, OnError);
+    }
+    void OnCharacterDataSend(UpdateUserDataResult result)
+    {
+        Debug.Log("Successful character data send!");
+    }
+    public void GetCharacterStats()
+    {
+        PlayFabClientAPI.GetUserData(new GetUserDataRequest(), OnCharacterDataReceived, OnError);
+    }
+    void OnCharacterDataReceived(GetUserDataResult result)
+    {
+        Debug.Log("Received character data!");
+        if(result.Data != null && result.Data.ContainsKey("Stats"))
+        {
+            game = GameObject.Find("GameContainer").GetComponent<Game>();      
+            character = JsonConvert.DeserializeObject<Character>(result.Data["Stats"].Value);
+            game.SetUIValues(character);
+            game.STRENGTH = character._STRENGTH;
+            game.AGILITY = character._AGILITY;
+            game.STAMINA = character._STAMINA;
+            game.KNOWLEDGE = character._KNOWLEDGE;
+            game.ABILITIES = character._ABILITIES;
+        }
     }
     #endregion
 }
